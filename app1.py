7015ca226f4ba19f805e67222452447fa43b1343
artemdatsenko <dacenko.aa@yandex.ru>
2024-05-24 00:12:07 +0300

    initial

--- app1.py
+++ app1.py
@@ -3,14 +3,14 @@ from minio import Minio
 from minio.error import S3Error
 
 
-# def upload_files(client, bucket_name, source_folder):
-#     # Проходим по всем директориям и файлам в указанной папке
-#     for root, dirs, files in os.walk(source_folder):
-#         for file in files:
-#             file_path = os.path.join(root, file)  # Полный путь к файлу
-#             object_name = os.path.relpath(file_path, source_folder)  # Относительный путь для объекта в бакете
-#             client.fput_object(bucket_name, object_name, file_path)  # Загрузка файла в бакет
-#             print(f"Загружен {file_path} как {object_name} в бакет {bucket_name}")
+def upload_files(client, bucket_name, source_folder):
+    # Проходим по всем директориям и файлам в указанной папке
+    for root, dirs, files in os.walk(source_folder):
+        for file in files:
+            file_path = os.path.join(root, file)  # Полный путь к файлу
+            object_name = os.path.relpath(file_path, source_folder)  # Относительный путь для объекта в бакете
+            client.fput_object(bucket_name, object_name, file_path)  # Загрузка файла в бакет
+            print(f"Загружен {file_path} как {object_name} в бакет {bucket_name}")
 
 
 def main():
@@ -53,39 +53,12 @@ def main():
             cut_date_load_file = date.strftime('%Y-%m-%d')[:10]
             cut_date_from_name_file = name_file[-14:-4]
             if cut_date_load_file != cut_date_from_name_file:
-                print('Даты не соответсвуют')
+                print('Даты не соответсвуют\n')
             else:
-                print('Даты соответсвуют')
+                print('Даты соответсвуют\n')
         else:
             print(name_file, ' - не .csv')
             print(date)
-        print('\n')
-    print('Файлы загруженные по указанной дате:\n')
-
-    # проверка файлов по указанной дате
-    objects2 = client.list_objects(bucket_name, recursive=True)
-    for o in objects2:
-        date = client.stat_object(bucket_name, o.object_name).last_modified
-        if find_time == date.strftime('%Y-%m-%d')[:10]:
-            name_file = o.object_name
-            if name_file.endswith('.csv'):
-                print(name_file)
-                print(date)
-                cut_date_load_file = date.strftime('%Y-%m-%d')[:10]
-                cut_date_from_name_file = name_file[-14:-4]
-
-                if cut_date_load_file != cut_date_from_name_file:
-                    print('Даты не соответсвуют')
-                else:
-                    print('Даты соответсвуют')
-            else:
-                print(name_file, ' - не .csv')
-                print(date)
-            print('\n')
-        else:
-            print('Таких файлов нет по указанной дате')
-            break
-    # проверка на существование во всех папках
 
 
 if __name__ == "__main__":
